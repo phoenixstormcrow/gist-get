aa38765ec26caf9360d7881f3592aa87da6f79ae
phoenixstormcrow <phoenixstormcrow@gmail.com>
2015-02-23 13:23:21 -0700

    Minor changes, manual testing.

---
+++ index.js
@@ -0,0 +1,55 @@
+/* gist.js
+    retrieves a gist using github api and request
+*/
+
+var request = require('request'),
+    gistURL = "https://api.github.com/gists/",
+    userAgent = "node-gist-get";
+
+
+function get(id, opts, callback) {
+    var ua = opts.userAgent || userAgent; // required by github api
+    request({ url: gistURL + id,
+              headers: { "User-Agent": ua }
+            },
+            function (error, response, body) {
+                var result;
+                if (error) {
+                    console.log("Get " + gistURL + id + " failed.");
+                    console.log(error);
+                    callback(error);
+                }
+                result = JSON.parse(body);
+                callback(null, result);
+            }
+    );
+}
+
+function getVersion(id, version, opts, callback) {
+    get(id + '/' + version, opts, callback);
+}
+
+function latest(id, opts, callback) {
+    get(id, opts, function (err, result) {
+        var history, latest;
+        if (err) callback(err);
+        if (result) {
+            history = result.history;
+            history.sort(function (a, b) {
+                // sort by committed_at date descending
+                a = new Date(a.committed_at);
+                b = new Date(b.committed_at);
+                if (a<b) return 1;
+                if (a>b) return -1;
+                else return 0;
+            });
+        latest = getVersion(id, history[0].version, opts, callback);
+        }
+    });
+}
+
+module.exports = {
+    get: get,
+    latest: latest,
+    version: getVersion
+};
